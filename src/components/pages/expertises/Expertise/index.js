0a38a0cce32a9d2309bda5ce194c80e10cbc3e8a
AdrienLapasset <aflapasset@gmail.com>
2023-05-25 10:29:20 +0200

    fix inactive nav zone

--- src/components/pages/expertises/Expertise/index.js
+++ src/components/pages/expertises/Expertise/index.js
@@ -8,11 +8,11 @@ import Cta from "components/global/Cta";
 import Dot from "components/global/Dot";
 
 const StyledContainer = styled.div`
-  padding-top: 150px;
-  border-top: ${(props) => props.theme.border.black};
   padding-top: 7px;
+  padding-bottom: 150px;
+  border-top: ${(props) => props.theme.border.black};
   @media ${(props) => props.theme.minWidth.md} {
-    padding-top: 130px;
+    padding-bottom: 130px;
   }
 `;
 const StyledPoint = styled.aside`
