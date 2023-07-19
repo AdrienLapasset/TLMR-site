738ec59e064bc1f9c5556d9a12381e9759661529
AdrienLapasset <aflapasset@gmail.com>
2023-07-19 16:04:56 +0200

    add video to formation page

--- src/pages/formations.js
+++ src/pages/formations.js
@@ -6,6 +6,7 @@ import TwoPointsSection from "components/global/TwoPointsSection";
 import { StaticImage } from "gatsby-plugin-image";
 import Seo from "components/Seo";
 import styled from "styled-components";
+import infolawersVideo from "assets/imgs/FORMATIONS/TLMR__info__02.mp4";
 
 const StyledPageHero = styled(PageHero)`
   padding-bottom: 40px;
@@ -31,6 +32,23 @@ const StyledContainer = styled.div`
     }
   }
 `;
+const StyledColumns = styled.div`
+  @media ${(props) => props.theme.minWidth.md} {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: ${(props) => props.theme.columnGap.mobile};
+  }
+  @media ${(props) => props.theme.minWidth.xl} {
+    grid-column-gap: ${(props) => props.theme.columnGap.desktop};
+  }
+  video {
+    max-width: 100%;
+    display: none;
+    @media ${(props) => props.theme.minWidth.md} {
+      display: block;
+    }
+  }
+`;
 
 const Formations = () => {
   return (
@@ -43,12 +61,15 @@ const Formations = () => {
               title="Formations"
               firstParagraph="Henri de la Motte Rouge et Jean Philippe Touati sont les premiers avocats infopreneurs. Ils ont fondé le Programme de formation en ligne Infolawyers. Dédié à l’accompagnement des entrepreneurs du web infopreneurs et e-commerçants, leur accompagnement inédit a permis à plusieurs centaines d’entrepreneurs d’être en conformité juridique de manière simple et rapide."
             />
-            <StaticImage
-              src="../assets/imgs/FORMATIONS/formation.jpg"
-              alt="TLMR - L’excellence accessible"
-              layout="fullWidth"
-              quality={100}
-            />
+            <StyledColumns>
+              <StaticImage
+                src="../assets/imgs/FORMATIONS/formation.jpg"
+                alt="TLMR - L’excellence accessible"
+              />
+              <video autoPlay muted loop playsInline>
+                <source src={infolawersVideo} type="video/mp4" />
+              </video>
+            </StyledColumns>
             <TwoPointsSection
               title1="VOUS PROTÉGER"
               description1=" Vous protéger en toute circonstance que ce soit pour relever vos défis d’entreprise ou en cas de coup dur. Le cabinet Touati La Motte Rouge Avocats répond à vos diverses problématiques avec rigueur et pragmatisme."
