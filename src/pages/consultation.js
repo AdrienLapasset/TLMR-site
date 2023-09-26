7a092879f18df8536a1fa9273f9679223b9dd1ef
AdrienLapasset <aflapasset@gmail.com>
2023-09-26 12:46:41 +0200

    add Layout to consultation page

--- src/pages/consultation.js
+++ src/pages/consultation.js
@@ -1,17 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import Layout from "components/Layout";
 
 const StyledIframe = styled.iframe`
   width: 100%;
+  margin-top: 72px;
   height: 100vh;
+  border: 0;
+  @media ${({ theme }) => theme.minWidth.md} {
+    margin-top: 0;
+  }
 `;
 
 const Consultation = () => {
   return (
-    <StyledIframe
-      title="consultation"
-      src="https://tlmr-avocats.kneo.me/iframe/56902376c9bd6ea.html"
-    ></StyledIframe>
+    <Layout>
+      <StyledIframe
+        title="consultation"
+        src="https://tlmr-avocats.kneo.me/iframe/56902376c9bd6ea.html"
+      ></StyledIframe>
+    </Layout>
   );
 };
 
